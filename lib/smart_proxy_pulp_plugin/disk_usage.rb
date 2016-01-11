8c525ada678db8069a0fa65a0fe2fb2250bee8b2
Ohad Levy <ohadlevy@gmail.com>
2016-01-11 12:05:32 +0200

    change path to accept multiple values, not tested!

--- lib/smart_proxy_pulp_plugin/disk_usage.rb
+++ lib/smart_proxy_pulp_plugin/disk_usage.rb
@@ -8,16 +8,13 @@ module PulpProxy
     def initialize(opts ={})
       @path = opts[:path] || raise(::Proxy::Error::ConfigurationError, 'Unable to continue - must provide a path.')
       @size = SIZE[opts[:size]] || SIZE[:gigabyte]
+      @stat = {}
       find_df
       get_stat
     end
 
     def to_json
-      data.to_json
-    end
-
-    def data
-      stat.merge({ :path => path, :size => SIZE.key(size) })
+      stat.to_json
     end
 
     private
@@ -28,14 +25,21 @@ module PulpProxy
       @command_path = which('df') || raise(::Proxy::Error::ConfigurationError, 'df command was not found unable to retrieve usage information.')
     end
 
+    def command
+      "#{command_path} -B#{size} #{path}"
+    end
+
     def get_stat
-      raw = Open3::popen3("#{command_path} -B#{size} #{path}") do |stdin, stdout, stderr, thread|
+      raw = Open3::popen3(command) do |stdin, stdout, stderr, thread|
         stdout.read.split("\n")
       end
 
-      titles = raw.first.downcase.gsub('mounted on', 'mounted').split.map(&:to_sym)
-      data   = raw.last.split
-      @stat  ||= Hash[titles.zip(data.map { |i| i })]
+      titles = raw.shift.downcase.gsub('mounted on', 'mounted').split.map(&:to_sym)
+      raw.each do |line|
+        values = line.split
+        mount_path = values[-1]
+        @stat[mount_path] = [titles.zip(values)]
+      end
     end
   end
 end
